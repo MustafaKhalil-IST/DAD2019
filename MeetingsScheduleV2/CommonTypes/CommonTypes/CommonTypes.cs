9169e1a9db5c17c4002681e2a7b15644aafcdead
MustafaKhalil-IST <mustafa.samir.khalil@tecnico.ulisboa.pt>
2019-11-02 20:57:51 +0000

    added version2 template

---
+++ MeetingsScheduleV2/CommonTypes/CommonTypes/CommonTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingsSchedule
+{
+    public class Class1
+    {
+    }
+}
